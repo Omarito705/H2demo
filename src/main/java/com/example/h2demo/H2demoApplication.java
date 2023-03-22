fb0d519fd7a86820f63cdfe8f33e3092b7c22e55
omar <omar.graouna@gmail.com>
2023-03-22 23:25:15 +0100

    Initial commit

---
+++ src/main/java/com/example/h2demo/H2demoApplication.java
@@ -0,0 +1,28 @@
+package com.example.h2demo;
+
+import com.example.h2demo.entities.Adherent;
+import com.example.h2demo.repository.AdherentRepository;
+import jdk.jfr.internal.Repository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class H2demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2demoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(AdherentRepository repository){
+        return args -> {
+            repository.save(new Adherent(null,"A","B",29));
+            repository.save(new Adherent(null,"A","B",29));
+            repository.save(new Adherent(null,"A","B",29));
+            repository.save(new Adherent(null,"A","B",29));
+
+        };
+    }
+}
